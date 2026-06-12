8a987fd34008a7d16bd08e78d08391cc56d6cda8
jbazan-bazia <jbazan@fi.uba.ar>
2026-06-12 15:56:17 -0300

    Cree estructura de tablas: Spots, Resportes, Restricciones. Con los respectivos campos que necesitaremos para el front end

--- backend de prueba (pokeAPI)/data/schemas.sql
+++ backend de prueba (pokeAPI)/data/schemas.sql
@@ -1,12 +1,28 @@
-CREATE TABLE tipos (
+CREATE TABLE spots (
     id SERIAL PRIMARY KEY,
-    nombre VARCHAR(100) UNIQUE NOT NULL
+    latitud DECIMAL(10, 8) NOT NULL,
+    longitud DECIMAL(11, 8) NOT NULL,
+    direccion_aproximada VARCHAR(100) NOT NULL,
+    estado_actual VARCHAR(50) NOT NULL DEFAULT 'ocupado',
+    ultima_actualizacion TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP 
 );
 
-CREATE TABLE pokemones (
+CREATE TABLE reportes (
     id SERIAL PRIMARY KEY,
-    nombre VARCHAR(100) UNIQUE NOT NULL,
-    evolucion INT NOT NULL,
-    tipo INT NOT NULL REFERENCES tipos(id),
-    rareza INT NOT NULL DEFAULT 0
+    spot_id INT REFERENCES spots(id),
+    estado_reportado VARCHAR(50) NOT NULL,
+    fecha_creacion TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    fecha_expiracion TIMESTAMP NOT NULL,
+    confianza_positiva INT NOT NULL DEFAULT 1,
+    confianza_negativa INT NOT NULL DEFAULT 0
+);
+
+CREATE TABLE restricciones (
+    id SERIAL PRIMARY KEY,
+    spot_id INT REFERENCES spots(id),
+    tipo_restriccion VARCHAR(100) NOT NULL,
+    dia_semana INT NOT NULL,
+    hora_inicio TIME NOT NULL,
+    hora_fin TIME NOT NULL,
+    descripcion TEXT
 );
